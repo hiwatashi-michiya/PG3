e5910211d1e4a073bf15fbe460ea07166f170ee2
k022g0063@g.neec.ac.jp <k022g0063@g.neec.ac.jp>
2023-10-02 12:45:07 +0900

    01_01確認課題

--- main.cpp
+++ main.cpp
@@ -17,18 +17,20 @@ char Min<char>(const char& a,const char& b) {
 
 	printf("数字以外の入力は受け付けません\n");
 
-	char result = ' ';
+	char result = '\0';
 
 	return result;
 
 }
+
+
 
 int main() {
 
 	printf("%d\n", Min<int>(14, 24));
 	printf("%f\n", Min<float>(25.3f, 11.2f));
 	printf("%lf\n", Min<double>(165.32f, 45.143f));
-	printf("%c\n", Min<char>('a', 'b'));
+	Min<char>('a', 'b');
 	
 	return 0;
 }

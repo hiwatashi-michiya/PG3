1b853deb3af935536a9c75378b395646b4d395ca
hiwatashi-michiya <115054875+hiwatashi-michiya@users.noreply.github.com>
2023-12-11 11:10:28 +0900

    一時保存

--- main.cpp
+++ main.cpp
@@ -1,9 +1,54 @@
 #include <stdio.h>
 #include <Windows.h>
+#include <list>
+#include <iostream>
+using namespace std;
 
 int main() {
 
 	SetConsoleOutputCP(65001);
+
+	list<const char*> yamanoteLine{
+		"Tokyo",
+		"Kanda",
+		"Akihabara",
+		"Okachimachi",
+		"Ueno",
+		"Uguisudani",
+		"Nippori",
+		/*"Nishi-Nippori",*/
+		"Tabata",
+		"Komagome",
+		"Sugamo",
+		"Otsuka",
+		"Ikebukuro",
+		"Mejiro",
+		"Takadanobaba",
+		"Shin-Okubo",
+		"Shinjuku",
+		"Yoyogi",
+		"Harajuku",
+		"Shibuya",
+		"Ebisu",
+		"Meguro",
+		"Gotanda",
+		"Osaki",
+		"Shinagawa",
+		/*"Takanawa Gateway",*/
+		"Tamachi",
+		"Hamamatsucho",
+		"Shimbashi",
+		"Yurakucho"
+	};
+
+	cout << "1970年\n";
+
+	for (auto it = yamanoteLine.begin(); it != yamanoteLine.end(); it++) {
+
+		cout << *it;
+		cout << "\n";
+
+	}
 
 	return 0;
 }
